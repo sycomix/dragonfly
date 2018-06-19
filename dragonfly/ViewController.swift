31c54937cfc15527e9fba957be6b583e95fd9dc4
Michael Williams <michael@michaeldwilliams.com>
2018-06-19 10:32:28 -0400

    project setup

--- dragonfly/ViewController.swift
+++ dragonfly/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import DJISDK
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        registerApp()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func registerApp() {
+        DJISDKManager.registerApp(with: self)
+    }
+
+}
+
+extension ViewController: DJISDKManagerDelegate {
+    func appRegisteredWithError(_ error: Error?) {
+        var message = "Register App Succeeded"
+        if error != nil {
+            message = "Register App Failed: Please enter your App key in the plist file and check the network."
+        } else {
+            print(message)
+        }
+        showAlertView(with: "Register App", message: message)
+    }
+
+    func showAlertView(with title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
 
